10a45cf8e8cf29641b12de561acfd1defed388ba
rdrworley <rdrworley@gmail.com>
2014-11-25 10:40:30 -0500

    From Home (11/25/2014): Adding Kemper data from Exp. 5 and 6 to m files

--- PFOA Adult Rat/Kemper Oral Gavage EXP 5.m
+++ PFOA Adult Rat/Kemper Oral Gavage EXP 5.m
@@ -1,9 +1,7 @@
-%This .m file runs the .csl file and overlays data from the Kemper, 2003 oral gavage experiments.
+%This .m file runs the .csl file and overlays data from the Kemper, 2003 oral gavage Experiment 5.
 
 output @clear
 prepare @clear @all
-
-!! s TSTOP = 24
 
 %liver binding parameters
 !! s Bmax = 1
@@ -15,47 +13,56 @@ prepare @clear @all
 
 !! st/nc
 
-%Experiment 1: 1 mg/kg administered via single oral dose. Whole blood and tisuses collected at sacrifice (672 hours - male, 168 hours - female)
-!! s BW = 0.233	%MALE
-%!! s BW = 0.175	%FEMALE
-!! s OdoseC = 1
-!! s IVdoseC = 0
-
-one_male_blood = [672	
-one_female_blood = [168
-plot(_year, _ca, one_male_blood(:,1), one_male_blood(:,2),'+')
-plot(_year, _ca, one_female_blood(:,1), one_female_blood(:,2),'+')
-
-%Experiment 2: 5 mg/kg administered via single oral dose. Whole blood and tisuses collected at sacrifice (672 hours - male, 168 hours - female)
-%!! s BW = 0.198	%MALE
-%!! s BW = 0.200	%FEMALE
-%!! s OdoseC = 5
-%!! s IVdoseC = 0
-
-%two_male_blood = [672	
-%two_female_blood = [168
-%plot(_year, _ca, two_male_blood(:,1), two_male_blood(:,2),'+')
-%plot(_year, _ca, two_female_blood(:,1), two_female_blood(:,2),'+')
-
-%Experiment 3: 25 mg/kg administered via single oral dose. Whole blood and tisuses collected at sacrifice (672 hours - male, 168 hours - female)
-%!! s BW = 0.198	%MALE
-%!! s BW = 0.200	%FEMALE
-%!! s OdoseC = 25
-%!! s IVdoseC = 0
-
-%three_male_blood = [672	
-%three_female_blood = [168
-%plot(_year, _ca, three_male_blood(:,1), three_male_blood(:,2),'+')
-%plot(_year, _ca, three_female_blood(:,1), three_female_blood(:,2),'+')
-
-
 %Experiment 5: 0.1 mg/kg administered via single oral dose. Whole blood collected from males at pre-dose, and 0.25, 0.5, 1,2,4,8, 12,16, 24 hours post-dose, 
 %and at 24-hour intervals through 192 hours. Whole blood collected from females at pre-dose, and at 0.25, 0.5, 1, 2, 4, 8, 16, 24, 36, 48, 72, and 96 hours 
 %post dose.
 
-%Experiment 6: 1 mg/kg administered via single oral dose. Whole blood collected from males at pre-dose, and 0.25, 0.5, 1,2,4,8, 12,16, 24 hours post-dose, 
-%and at 24-hour intervals through 192 hours. Whole blood collected from females at pre-dose, and at 0.25, 0.5, 1, 2, 4, 8, 16, 24, 36, 48, 72, and 96 hours 
-%post dose.   
+!! s TSTOP = 550	% MALE
+!! s BW = 0.216	%MALE
+
+%!! s TSTOP = 100	%FEMALE
+%!! s BW = 0.176	%FEMALE
+
+!! s OdoseC = 0.1	%mg/kg BW
+!! s IVdoseC = 0
+
+five_male_blood = [0	0
+0.25	0.282
+0.5	0.302
+1.0	0.376
+2.0	0.388
+4.0	0.404
+8.0	0.467
+12.0	0.517
+16.0	0.511
+24.0	0.399
+36.0	0.364
+48.0	0.328
+72.0	0.324
+96.0	0.311
+120.0	0.262
+144.0	0.251
+168.0	0.253
+192.0	0.225
+240.0	0.177
+288.0	0.181
+336.0	0.125
+384.0	0.108
+432.0	0.100
+480.0	0.087
+528.0	0.063]	
+
+five_female_blood = [0.0	0
+0.25	0.603
+0.50	0.630
+1.0	0.539
+2.0	0.528
+4.0	0.381
+8.0	0.102
+16.0	0.050]
+
+plot(_year, _ca, five_male_blood(:,1), five_male_blood(:,2),'+')		%MALE
+%plot(_year, _ca, five_female_blood(:,1), five_female_blood(:,2),'+')		%FEMALE
 
 %Experiment 7: 5 mg/kg administered via single oral dose. Whole blood collected from males at pre-dose, and 0.25, 0.5, 1,2,4,8, 12,16, 24 hours post-dose, 
 %and at 24-hour intervals through 192 hours. Whole blood collected from females at pre-dose, and at 0.25, 0.5, 1, 2, 4, 8, 16, 24, 36, 48, 72, and 96 hours 
---
+++ PFOA Adult Rat/Kemper Oral Gavage EXP 6.m
@@ -0,0 +1,69 @@
+%This .m file runs the .csl file and overlays data from the Kemper, 2003 oral gavage Experiment 6.
+
+output @clear
+prepare @clear @all
+
+%liver binding parameters
+!! s Bmax = 1
+!! s Kb = 1
+!! s koff = 1
+
+%kidney parameters
+!! s keffluxc = 0.1
+
+!! st/nc
+
+%Experiment 6: 1 mg/kg administered via single oral dose. Whole blood collected from males at pre-dose, and 0.25, 0.5, 1,2,4,8, 12,16, 24 hours post-dose, 
+%and at 24-hour intervals through 192 hours. Whole blood collected from females at pre-dose, and at 0.25, 0.5, 1, 2, 4, 8, 16, 24, 36, 48, 72, and 96 hours 
+%post dose.   
+
+!! s TSTOP = 550	% MALE
+!! s BW = 0.249	%MALE
+
+%!! s TSTOP = 100	%FEMALE
+%!! s BW = 0.197	%FEMALE
+
+!! s OdoseC = 1.0	%mg/kg BW
+!! s IVdoseC = 0
+
+six_male_blood = [0	0
+0.25	2.739
+0.5	3.727
+1.0	4.465
+2.0	5.558
+4.0	7.041
+8.0	7.363
+12.0	7.066
+16.0	7.229
+24.0	4.976
+36.0	4.706
+48.0	4.295
+72.0	3.471
+96.0	3.378
+120.0	3.599
+144.0	2.828
+168.0	2.785
+192.0	2.996
+240.0	1.488
+288.0	1.211
+336.0	1.205
+384.0	1.089
+432.0	0.770
+480.0	0.779
+528.0	0.447]	
+
+six_female_blood = [0.0	0
+0.25	4.116
+0.50	4.552
+1.0	4.723
+2.0	4.389
+4.0	3.801
+8.0	1.452
+16.0	0.689
+24.0	0.159
+36.0	0.029
+48.0	0.036
+96.0	0.075]
+
+plot(_year, _ca, six_male_blood(:,1), six_male_blood(:,2),'+')		%MALE
+%plot(_year, _ca, six_female_blood(:,1), six_female_blood(:,2),'+')		%FEMALE
