e86275b8415ecf597a3a5706260e503c75701b7f
rdrworley <rdrworley@gmail.com>
2015-01-08 16:49:46 -0500

    From Home (1/8/2015): Oral gavage studies look pretty good. Need to deal with IV.

--- PFOA Adult Rat/Kudo IV low dose 2h.m
+++ PFOA Adult Rat/Kudo IV low dose 2h.m
@@ -7,7 +7,7 @@ WESTIG=0;WEDITG=0;CJVITG=0;
 %liver binding parameters (Values from Loccisano PFOS model, Bmax is female values)
 !! s Bmax = 0
 !! s koff = 0
-!! s kbilec = 0.0006
+!! s kbilec = 0.0008
 !! s kabs = 0.35
 !! s kunabs = 0.0001
 !! s k0c = 1.0e-33
@@ -15,7 +15,7 @@ WESTIG=0;WEDITG=0;CJVITG=0;
 
 %kidney parameters
 !! s keffluxc = 10
-!! s kurinec = 1.3
+!! s kurinec = 2.5
 
 %Experiment: 0.041 mg/kg BW administered to male wistar rats in single bolus IV, serum and liver concentrations measured 2h post-dose serum collected at time points from 0-300 minutes.
 
