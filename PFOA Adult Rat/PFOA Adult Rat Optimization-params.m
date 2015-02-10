ef66586d433f37ed67310cc88f50c3a95bbf2953
rdrworley <rdrworley@gmail.com>
2015-02-10 15:11:09 -0500

    From Home: Optimizing IV parameters

--- PFOA Adult Rat/PFOA Adult Rat Optimization-params.m
+++ PFOA Adult Rat/PFOA Adult Rat Optimization-params.m
@@ -1,8 +1,8 @@
 % Automatically generated through parameter estimation script 'PFOA Adult Rat Optimization.m'
 
 K0C = 1.000000e+00;
-KABS = 1.900000e+00;
+KABS = 3.000000e+00;
 KUNABS = 1.000000e-03;
-KBILEC = 4.800000e-03;
-KEFFLUXC = 1.000000e+01;
-KURINEC = 1.560000e+00;
+KBILEC = 8.120272e-03;
+KEFFLUXC = 3.600000e+01;
+KURINEC = 4.000000e-04;
