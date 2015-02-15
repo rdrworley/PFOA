026428b591a03138768bc660b4282dc8876c5851
rdrworley <rdrworley@gmail.com>
2015-02-15 10:04:59 -0500

    From Home (2/15/2015): Exploring dose repsonse in kidney

--- PFOA Adult Rat/PFOA Adult Rat Test.m
+++ PFOA Adult Rat/PFOA Adult Rat Test.m
@@ -4,31 +4,18 @@ output @clear
 prepare @clear @all
 WESTIG=0;WEDITG=0;CJVITG=0;
 
-%liver binding parameters (Values from Loccisano PFOS model, Bmax is female values)
-!! s Bmax = 0
-!! s koff = 0
-!! s kbilec = 0.004
-!! s kabs = 3.0
-!! s kunabs = 0.0001
-!! s k0c = 1.0
-!! s GEC = 0.54
+!! s Vmax_baso_invitro = 393.45
+!! s Vmax_apical = 18861.8
+!! s Km_baso = 37.7
+!! s Km_apical = 141
+RAFbaso = 0.01356
+RAFapi = 0.01356
 
-%kidney parameters
-!! s keffluxc = 10
-!! s kurinec = 1.6
+!! s TSTOP = 530.0
 
-%GI Tract Parameters
-!! s GEC = 0.54
-
-%IV dosing parameters
-!! s IVTime = 0.1
-
-
-!! s TSTOP = 1.0
-
-!! s OdoseC = 1.0e-33	!mg/kg BW
-!! s IVdoseC = 1.0
+!! s OdoseC = 2000 !mg/kg BW
+!! s IVdoseC = 0.0
 
 !! st/nc
 
-!! display CA, RPlas_free, Aplas_free, CA_free, QR, CVR, QK, CVK, QL, CVL, QC, Atissue, Aloss, Atotal, AIV
+!! display CA, Adif, ACl, A_baso, A_apical, Aefflux, Aurine
