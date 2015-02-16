013161488dd24ef379f2780dce457eefb4331539
unknown <idz7@L309348.cdc.gov>
2015-02-16 11:56:10 -0500

    From Work (2/16/2015): Exploring the role of the kidney, impact of transporter code.

--- PFOA Adult Rat/PFOA Adult Rat Test.m
+++ PFOA Adult Rat/PFOA Adult Rat Test.m
@@ -4,8 +4,8 @@ output @clear
 prepare @clear @all
 WESTIG=0;WEDITG=0;CJVITG=0;
 
-!! s Vmax_baso_invitro = 393.45
-!! s Vmax_apical = 18861.8
+!! s Vmax_baso_invitro = 0
+!! s Vmax_apical_invitro = 0
 !! s Km_baso = 37.7
 !! s Km_apical = 141
 RAFbaso = 0.01356
@@ -13,9 +13,10 @@ RAFapi = 0.01356
 
 !! s TSTOP = 530.0
 
-!! s OdoseC = 2000 !mg/kg BW
+!! s OdoseC = 1000 !mg/kg BW
 !! s IVdoseC = 0.0
 
 !! st/nc
 
 !! display CA, Adif, ACl, A_baso, A_apical, Aefflux, Aurine
+plot (_t, _ca)
