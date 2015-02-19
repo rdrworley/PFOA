f355f52e75890c3d733065fa7e5e0c7cfaee2e13
rdrworley <rdrworley@gmail.com>
2015-02-19 17:12:02 -0500

    From Home (2/19/2015): Exploring the role of the kidney. Results sent to Jeff.

--- PFOA Adult Rat/PFOA Adult Rat Test.m
+++ PFOA Adult Rat/PFOA Adult Rat Test.m
@@ -4,21 +4,21 @@ output @clear
 prepare @clear @all
 WESTIG=0;WEDITG=0;CJVITG=0;
 
-%!! s Vmax_baso_invitro = 393.45	 %averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007
+%!! s Vmax_baso_invitro = 393.45	 %averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007 (initial)
 %!! s Vmax_baso_invitro = 423.3	 %averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007 (uper bound)
 !! s Vmax_baso_invitro = 363.6	 %averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007 (lower bound)
 %!! s Vmax_baso_invitro = 0.0	 %averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007 (TURNED OFF)
 
-%!! s Vmax_apical_invitro = 9300	%invitro value for Oatp1a1 from Weaver, 2010
+%!! s Vmax_apical_invitro = 9300	%invitro value for Oatp1a1 from Weaver, 2010 (initial)
 %!! s Vmax_apical_invitro = 10700	%invitro value for Oatp1a1 from Weaver, 2010 (uper bound)
 !! s Vmax_apical_invitro = 7900	%invitro value for Oatp1a1 from Weaver, 2010 (lower bound)
 %!! s Vmax_apical_invitro = 0.0	%invitro value for Oatp1a1 from Weaver, 2010 (TURNED OFF)
 
-%!! s Km_baso = 27.2	%average of OAT1 and OAT3 from Nakagawa et. al, 2007
+%!! s Km_baso = 27.2	%average of OAT1 and OAT3 from Nakagawa et. al, 2007 (initial)
 %!! s Km_baso = 33.3	%average of OAT1 and OAT3 from Nakagawa et. al, 2007 (uper bound)
 !! s Km_baso = 21.0 	%average of OAT1 and OAT3 from Nakagawa et. al, 2007 (lower bound)
 
-%!! s Km_apical = 52.3	%invitro value for Oatp1a1 from Weaver, 2010
+%!! s Km_apical = 52.3	%invitro value for Oatp1a1 from Weaver, 2010 (initial)
 %!! s Km_apical = 62.2	%invitro value for Oatp1a1 from Weaver, 2010 (uper bound)
 !! s Km_apical = 42.4	%invitro value for Oatp1a1 from Weaver, 2010 (lower bound)
 
@@ -32,5 +32,8 @@ RAFapi = 0.01356
 
 !! st/nc
 
-!! display CA, Adif, ACl, A_baso, A_apical, Aefflux, Aurine
+max(_ca)
+%max(_ca)/2
+
+!! display  A_baso, A_apical, TMAX
 plot (_t, _ca)
