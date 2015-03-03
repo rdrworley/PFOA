34f3f14aec79392c87ffd4b88c52fc296dfa941d
rdrworley <rdrworley@gmail.com>
2015-03-03 17:43:30 -0500

    From Home (3/3/2015): Sensitivity analysis

--- PFOA Adult Rat/Optimization.m
+++ PFOA Adult Rat/Optimization.m
@@ -1,8 +1,58 @@
-!! s K0C = 1.0
-!! s KABS = 3.0
-!! s KUNABS = 0.0001
+output @clear
+prepare @clear @all
 
-!! s KBILEC = 0.004
-!! s KEFFLUXC = 10
-!! s KURINEC = 1.6
+% Physiological Parameters
+!! s BW = 0.3	
+!! s MKC = 0.0084	
+!! s MLC = 0.034	
 
+% Cardiac Output and Bloodflow 
+!! s QCC = 14.0 	 
+!! s QLC = 0.183 
+!! s QKC = 0.141 
+!! s Htc = 0.46 
+		
+% Tissue Volumes 
+!! s VplasC = 0.0312 
+!! s VLC = 0.035 
+!! s VKC = 0.0084 
+!! s VfilC = 8.4e-4	
+!! s VPTCC = 1.35e-4 
+	
+% Chemical Specific Parameters
+!! s MW = 414.07	
+
+%Free Fraction of Chemical in Plasma
+!! s Free = 0.006 
+		
+%Kidney Transport Parameters
+!! s Vmax_baso_invitro = 393.45 
+!! s Km_baso = 27.2 
+!! s Vmax_apical_invitro = 9300 
+!! s Km_apical = 52.3 
+!! s RAFbaso = 0.01356	
+!! s RAFapi = 0.01356	
+!! s protein = 2.0e-6	
+
+!! s GFRC = 62.1	
+
+%Partition Coefficients (from A. Locissano, from rat tissue data, Kudo et al, 2007)
+!! s PL = 2.2 
+!! s PK = 1.05 
+!! s PR = 0.11 
+		
+%parameters for binding in liver (10/5/2014 - all set to zero per Locissano, 2012. Will need to reevaluate.)
+!! s Kb = 1  
+!! s Bmax = 1  
+!! s koff= 1 
+ 
+%rate constants (1/h)
+!! s kdif = 0.001	
+!! s kabs = 3.0 
+!! s kunabs = 0.0001 
+!! s GEC = 0.54 
+!! s k0C = 1.0	
+		
+!! s keffluxc = 10	
+!! s kbilec = 0.004 
+!! s kurinec = 1.6 
