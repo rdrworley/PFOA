57149b51bfef8818998670a391911dfdaa893cd8
rdrworley <rdrworley@gmail.com>
2015-03-17 16:32:08 -0400

    From Home (3/17/2015): Working on the female

--- PFOA Adult Rat/Kemper Oral Gavage EXP 6 female.m
+++ PFOA Adult Rat/Kemper Oral Gavage EXP 6 female.m
@@ -12,7 +12,13 @@ prepare @clear @all
 !! s KEFFLUXC = 10
 !! s KURINEC = 1.6	%Locissano uses 0.32, fit to data
 
-!! s Free = 0.045	%Female value for FREE used in Locissano model (fit to data).
+%Kidney Transport Parameters
+!! s Vmax_baso_invitro = 393.45 %Vmax of basolateral transporter (pmol/mg protein/min); averaged in vitro value of OAT1 and OAT3 from Nakagawa, 2007
+!! s Km_baso = 27.2 %Km of basolateral transporter (ug/mL) Average of OAT1 and OAT3 from Nakagawa et. al, 2007
+!! s Vmax_apical_invitro = 9300 %Vmax of apical transporter (pmol/mg protein/min); invitro value for Oatp1a1 from Weaver, 2010
+!! s Km_apical = 52.3 %Km of apical transporter (ug/mL), in vitro value for Oatp1a1 from Weaver, 2010.
+!! s RAFbaso = 0.01356	%relative activity factor, basolateral transporters (averaged from OAT1 and OAT3 in Yamada, 2007)
+!! s RAFapi = 0.0001356	%relative activity factor, apical transporters (NEED VALUE - current value is copied from RAF for baso)
 !! s GFRC = 41.04	%glomerular filtration rate (L/hr/kg kidney) (female); Corley, 2005
 
 %Experiment 6: 1 mg/kg administered via single oral dose. Whole blood collected from males at pre-dose, and 0.25, 0.5, 1,2,4,8, 12,16, 24 hours post-dose, 
