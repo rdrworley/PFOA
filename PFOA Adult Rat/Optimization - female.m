0c1cb3b2f77f8f095def32cc07d8e9fee3836177
rdrworley <rdrworley@gmail.com>
2015-03-18 15:20:01 -0400

    From Home (3/18/2015)

---
+++ PFOA Adult Rat/Optimization - female.m
@@ -0,0 +1,6 @@
+output @clear
+prepare @clear @all
+
+!! s KEFFLUXC = 10
+!! s KURINEC = 1.6	%Locissano uses 0.32, fit to data
+!! s RAFapi = 0.01356	%relative activity factor, apical transporters (NEED VALUE - current value is copied from RAF for baso)
