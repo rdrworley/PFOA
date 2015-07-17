e533aaa193e22ff520015f7d9697d2b967e6b6dd
unknown <idz7@L309348.cdc.gov>
2015-07-17 15:41:12 -0400

    From Work (7/17/2015): Completely reworked human model.

--- Project 1/test.m
+++ Project 1/test.m
@@ -11,17 +11,11 @@ WESTIG=0;WEDITG=0;CJVITG=0;
 !! s tstop = 96360
 
 %Fit-to-data parameters
-!! s K0C = 1.0
-!! s KABSC = 2.12
-!! s KUNABSC = 7.06e-5
+!! s KEFFLUXC = 2.49
+!! s KURINEC = 1.6
 
-!! s KBILEC = 0.004
-!! s KEFFLUXC = 2.49e-4
-!! s KURINEC = 1.6e-6
-!! s Km_apical = 128500 %Value for OAT4 only (.csl has average of OAT4 and URAT1)
-
-!! s RAFbaso = 1.356e-8
-!! s RAFapi = 1.356e-7		
+!! s RAFbaso = 0.01356
+!! s RAFapical = 0.01356e	
 
 %Data
 bartell = [4	91
