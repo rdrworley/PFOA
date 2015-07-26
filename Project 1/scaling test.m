c6ff752d5458d66cfa171141518081df8fdcc524
rdrworley <rdrworley@gmail.com>
2015-07-26 12:16:44 -0400

    From Home (7/26/2015). Reworked model, pure scale from rat model. Performance is better, but may be insensitive again.

---
+++ Project 1/scaling test.m
@@ -0,0 +1,19 @@
+output @clear
+prepare @clear @all
+WESTIG=0;WEDITG=0;CJVITG=0;
+
+!! s K0C = 1.0
+!! s KABSC = 2.12
+!! s KUNABSC = 7.06e-5
+
+!! s KBILEC = 0.004
+!! s KEFFLUXC = 2.49
+!! s KURINEC = 1.6
+
+% Exposure parameters
+!! s tbackground = 0
+!! s backgrounddw = 0.01
+!! s exposeddw = 3.55
+%!! s tstop = 219000
+
+!! st/nc
