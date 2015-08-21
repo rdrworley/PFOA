ef940e65b07259d6bc794d7dc850e06bb599aa60
rdrworley <rdrworley@gmail.com>
2015-08-21 17:22:32 -0400

    From Home (8/21/2015): TURNING OFF BILIARY EXCRETION WORKED!

--- Project 1/Little Hocking.m
+++ Project 1/Little Hocking.m
@@ -5,22 +5,22 @@ prepare @clear @all
 WESTIG=0;WEDITG=0;CJVITG=0;
 
 %Exposure Parameters
-!! s exposeddw = .19
-!! s tstop = 87600
+!! s exposeddw = 3.5
+!! s tstop = 175200
 
 %Parameters that can be fit to data
 !! s K0C = 1.0
 !! s KABSC = 2.12
 !! s KUNABSC = 7.06e-5
 
-!! s KBILEC = 0.004
+!! s KBILEC = 0
 !! s KEFFLUXC = 2.49
 !! s KURINEC = 1.6
 
 %!! s Km_baso = 20.1
 %!! s Km_apical = 77.5
 
-!! s RAFapi =  0.1356
+!! s RAFapi =  0.035
 !! s RAFbaso = 0.01356
 
 !! s Free = 0.02
@@ -30,27 +30,27 @@ WESTIG=0;WEDITG=0;CJVITG=0;
 %!! s PK = 0.1
 
 %Data (serum concentrations reported in ug/L)
-bartell = [4	91
-4	424
-4	757]
+bartell = [15	91
+15	424
+15	757]
 
-steenland = [6	130
-6	227
-6	318]
+steenland = [17	130
+17	227
+17	318]
 
-emmett = [8	221
-8	374
-8	448
-8	576]
+emmett = [19	221
+19	374
+19	448
+19	576]
 % 373 is median, 447 is mean
 
-lhwa = [10	147
-10	343
-10	540]
+lhwa = [13	147
+13	343
+13	540]
 
 !! st/nc
 
-%plot(_year, _ca, steenland(:,1),steenland(:,2),'+', bartell(:,1),bartell(:,2),'+', emmett(:,1),emmett(:,2),'+', lhwa(:,1), lhwa(:,2),'+')
+plot(_year, _ca, steenland(:,1),steenland(:,2),'+', bartell(:,1),bartell(:,2),'+', emmett(:,1),emmett(:,2),'+', lhwa(:,1), lhwa(:,2),'+')
 
-plot(_year, _ca)
-plot(_year, _cfil)
+%plot(_year, _ca)
+%plot(_year, _cfil)
